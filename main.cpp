4c7765ad518e671f4f6eeb39d0ee84f1e772a2ac
jessicasinger <singer.jeess@gmail.com>
2017-08-30 21:32:13 -0300

    nose

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ bool habloMalDeUnBueno(std::set<int> &agentesConfiables, Opinion &opinion, int a
 }
 bool unBuenoHabloMalDeEl(std::set<int> &agentesConfiables, Opinion& opinion, int actual, bool confio)
 {
-    return (agentesConfiables.find(opinion.emisor) != agentesConfiables.end()) && (opinion.receptor == actual) &&
+    return ((agentesConfiables.find(opinion.emisor) != agentesConfiables.end()) || opinion.emisor == actual) && (opinion.receptor == actual) &&
            (opinion.confioEnReceptor == 0) && confio;
 }
 bool habloBienDeUnMalo(std::set<int> &agentesConfiables, Opinion& opinion, int actual, bool confio)
@@ -95,7 +95,7 @@ bool tieneSentido(std::set<int> &agentesConfiables, std::vector<Opinion> &vector
         iterator = BuenosParaElActual.begin();
         while (iterator != BuenosParaElActual.end())
         {
-            if (MalosPosta.find(*iterator) != BuenosPosta.end()){
+            if (MalosPosta.find(*iterator) != MalosPosta.end()){
                 return false;
             }
             iterator++;
@@ -104,46 +104,54 @@ bool tieneSentido(std::set<int> &agentesConfiables, std::vector<Opinion> &vector
     return true;
 }
 int generarSolucion( std::vector<Opinion> &vectorcitoDeOpiniones,
-                     std::set<int> &agentesConfiables, int i, int n)
+                     std::set<int> &agentesConfiables, int i, int n, int &maximoParcial)
 {
     if (0 > i) {
           if (agentesConfiables.size() > m) {
-              m = agentesConfiables.size();
+              maximoParcial = agentesConfiables.size();
           }
           return agentesConfiables.size();
     }
     int sinElla = -1;
     int conElla = -1;
 
-    if ((agentesConfiables.size() - i) < m){ //los q ya agregue + los q me faltan procesar
-        return -1;
-    } else {
 
     bool tieneSentidoAgregarlo = tieneSentido(agentesConfiables, vectorcitoDeOpiniones, i, 1);
     bool tieneSentidoNoAgregarlo = tieneSentido(agentesConfiables, vectorcitoDeOpiniones, i, 0);
 
-    if (tieneSentidoAgregarlo){
+    if (tieneSentidoAgregarlo && ((agentesConfiables.size() + i) >= maximoParcial)){
         agentesConfiables.insert(i);
-        conElla = generarSolucion(vectorcitoDeOpiniones, agentesConfiables, i-1, n);
+        conElla = generarSolucion(vectorcitoDeOpiniones, agentesConfiables, i-1, n, maximoParcial);
         agentesConfiables.erase(i);
     }
-     if (tieneSentidoNoAgregarlo) {
-            sinElla = generarSolucion(vectorcitoDeOpiniones, agentesConfiables, i-1, n);
+     if (tieneSentidoNoAgregarlo && ((agentesConfiables.size() + i - 1) >= maximoParcial)) {
+            sinElla = generarSolucion(vectorcitoDeOpiniones, agentesConfiables, i-1, n, maximoParcial);
     }
 
         return std::max(sinElla, conElla);
     }
-}
+
 
 
 std::vector<Opinion>  generarMejorInstancia(int n) {
     std::vector<Opinion> opiniones;
     for (int j = 1; j <= n; j++)
     {
-       // for (int i = 0; i < n; i++){
-            Opinion opinion = {j-1, j, 0};
-            opiniones.push_back(opinion);
+        // for (int i = 0; i < n; i++){
+        Opinion opinion = {j-1, j, 0};
+        opiniones.push_back(opinion);
         //}
+    }
+    return opiniones;
+}
+std::vector<Opinion>  generarOtraMejorInstancia(int n) {
+    std::vector<Opinion> opiniones;
+    for (int j = 0; j < n; j++)
+    {
+         for (int i = 0; i < n; i++){
+        Opinion opinion = {i, j, 0};
+        opiniones.push_back(opinion);
+        }
     }
     return opiniones;
 }
@@ -159,7 +167,26 @@ std::vector<Opinion> generarPeorInstancia(int n)
     }
     return opiniones;
 }
+std::vector<Opinion> generarAlgunaInstancia()
+{
+    std::vector<Opinion> opiniones;
+   // opiniones.push_back({9, 12, 1});
+    opiniones.push_back({0, 5, 1});
+    opiniones.push_back({0, 1, 1});
+    opiniones.push_back({3, 2, 1});
+    opiniones.push_back({4, 2, 1});
+    opiniones.push_back({4, 0, 1});
+    opiniones.push_back({5, 6, 0});
+    opiniones.push_back({6, 3, 0});
+    opiniones.push_back({7, 6, 1});
+    opiniones.push_back({7, 0, 1});
+    opiniones.push_back({8, 1, 0});
+    opiniones.push_back({9, 8, 1});
+    opiniones.push_back({9, 4, 1});
+    opiniones.push_back({9, 2, 0});
 
+  return opiniones;
+}
 int main() {
 
     std::string nombreArchivo = "esidatos";
@@ -169,17 +196,19 @@ int main() {
     std::ofstream a_file (ss.str());
 
     a_file << "long,tiempo" << std::endl;
-    int minN = 1, maxN = 40, saltarDeA = 1, cantInstanciasPorN = 3;
+    int minN = 1, maxN = 1000, saltarDeA = 3, cantInstanciasPorN = 5;
     for (int i = minN; i <= maxN; i+=saltarDeA) {
         long long tiempoTotal = 0;
         for (int j = 0; j < cantInstanciasPorN; ++j) {
 
             std::set<int> agentesConfiables;
-            std::vector<Opinion> vectorcitoDeOpiniones = generarMejorInstancia(i);
+            std::vector<Opinion> vectorcitoDeOpiniones = generarAlgunaInstancia();//= generarOtraMejorInstancia(i);
 //            std::cout << vectorcitoDeOpiniones;
 
             auto tpi = std::chrono::high_resolution_clock::now();
-            int rta = generarSolucion(vectorcitoDeOpiniones, agentesConfiables, i-1, i);
+            //int maximoParcial = -1;
+            //int rta = generarSolucion(vectorcitoDeOpiniones, agentesConfiables, i-1, i, m);
+            int rta = generarSolucion(vectorcitoDeOpiniones, agentesConfiables, 9, 10, m);
             m = 0;
             auto tpf = std::chrono::high_resolution_clock::now();
 
